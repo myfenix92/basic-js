f74406f98da925b6f4314d4d5f3d6330843f5a2f
Yana Kornejkova <bloom.hog@gmail.com>
2020-03-09 20:33:11 +0300

    feat: add task Dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,12 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  //throw 'Not implemented'
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let str = ''
+  let membersName = members.filter(item => typeof(item) === 'string')
+  for (i = 0; i < membersName.length; i++) {
+    str = str + membersName[i].toString().trim().slice(0,1)
+  }
+  return str.toUpperCase().split('').sort().join('')
 };
