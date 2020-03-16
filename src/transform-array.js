8968314bf6a8e05becec4e4f22863c26ec61bcc6
Yana Kornejkova <bloom.hog@gmail.com>
2020-03-17 00:18:23 +0300

    feat: add task Transform Array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,32 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    //throw 'Not implemented';
+    if (!Array.isArray(arr)) throw new Error()
+    
+    let newArr = []
+    
+    for (i = 0; i < arr.length; i++) {
+        if( arr[i] === '--discard-next') {
+            i++ 
+            continue
+        } 
+
+        if( arr[i] === '--discard-prev') {
+            newArr.pop()
+            continue
+        }
+
+        if( arr[i] === '--double-next') {
+            newArr.push(arr[i+1])
+            continue
+        }
+    
+        if( arr[i] === '--double-prev') {
+            newArr.push(arr[i-1])
+            continue
+        }
+        newArr.push(arr[i])
+    }
+    newArr = newArr.filter( item => item != undefined)
+
+    return newArr
 };
