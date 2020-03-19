733c7956062966753ff601cfd19b8066d22c6985
Yana Kornejkova <bloom.hog@gmail.com>
2020-03-19 19:29:53 +0300

    feat: add task Count Cat

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,9 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  //throw 'Not implemented';
+  let n = 0
+  matrix = matrix.flat()
+  matrix.forEach((el) => {
+    if(el === '^^') n = n + 1
+  });
+  return n
 };
