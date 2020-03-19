bb60810473ba178bd241c9ee45980f4663b4cb01
Yana Kornejkova <bloom.hog@gmail.com>
2020-03-19 20:42:25 +0300

    feat: add task Carbon Dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,12 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  //throw 'Not implemented';
+  let n
+  if (typeof(sampleActivity) === 'string' && parseFloat(sampleActivity) > 0 && parseFloat(sampleActivity) < 15 && parseFloat(sampleActivity) !== NaN)
+  {
+    n = Math.ceil((HALF_LIFE_PERIOD * Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity))) / 0.693)
+    return n
+  } else {return false}
 };
