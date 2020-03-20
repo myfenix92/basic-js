a244c8463a42a1f5d11e1c53b72d59df79d0e5eb
Yana Kornejkova <bloom.hog@gmail.com>
2020-03-20 21:22:39 +0300

    feat: add task Simple Chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,30 @@
 const chainMaker = {
+  chainCur: [],
+
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chainCur.length
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chainCur.push('( ' + value + ' )')
+    return this
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (position <= 0 || (position ^ 0) !== position || typeof (position) !== 'number' || position > this.getLength() || position === '') {
+      this.chainCur = []
+      throw Error()
+    } else {
+      this.chainCur.splice(position - 1, 1)
+      return this
+    }
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chainCur.reverse()
+    return this
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let newChain = this.chainCur.join('~~')
+    this.chainCur = []
+    return newChain
   }
 };
 
