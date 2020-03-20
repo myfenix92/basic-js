e269ad63aaf50b483dac087984ef819e5c14b2e1
Yana Kornejkova <bloom.hog@gmail.com>
2020-03-20 19:18:52 +0300

    feat: add task Extended Repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,41 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    //throw 'Not implemented';
+    let repeatTimesKey = 'repeatTimes'
+    let separatorKey = 'separator'
+    let additionKey = 'addition'
+    let additionRepeatTimesKey = 'additionRepeatTimes'
+    let additionSeparatorKey = 'additionSeparator'
+    let times = options[repeatTimesKey]
+    let separator = options[separatorKey]
+    let addition = options[additionKey]
+    let additionRepeat = options[additionRepeatTimesKey]
+    let additionSeparator = options[additionSeparatorKey]
+
+    if (times === undefined) {
+        str = str + addition
+    } else {
+
+        if (options.separator === undefined) {
+            separator = '+'
+        }
+
+        if (options.addition === undefined) {
+            addition = ''
+        }
+
+        if (options.additionRepeat === null) {
+            additionRepeat = 0
+        }
+
+        if (options.additionSeparator === undefined) {
+            additionSeparator = ''
+        }
+
+        addition = (addition + additionSeparator).repeat(additionRepeat)
+        addition = addition.slice(0, (addition.length - additionSeparator.length))
+        str = (str + addition + separator).repeat(times)
+        str = str.slice(0, (str.length - separator.length))
+    }
+
+    return str
 };
-  
